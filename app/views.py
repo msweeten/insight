c17122e690e7b1d354d57ff1c24ee91b827d6e5a
msweeten <msweeten43@gmail.com>
2017-02-02 17:57:52 -0500

    Modified app dataset and added spotify play

--- app/views.py
+++ app/views.py
@@ -1,43 +1,169 @@
 from flask import render_template
 from app import app
+import psycopg2
+import pandas as pd
+import json
 
+app.config.from_pyfile('/home/msweeten/insight/Config.py')
+
+print(app.config['DB_HOST'])
+
+def connect():
+    db_connect = psycopg2.connect(
+        database = app.config['DATABASE'],
+        user = app.config['DB_ID'],
+        host = app.config['DB_HOST'],
+        password = app.config['DB_PW']
+    )
+    return db_connect
+
+class mydict(dict):
+    def __str__(self):
+        return json.dumps(self)
+
+base = "https://embed.spotify.com/?uri=" 
 @app.route('/')
 def main():
-    return(render_template('home.html'))
+    return render_template('home.html')
 @app.route('/home.html')
 def home():
-    return(render_template('home.html'))
+    return render_template('home.html')
 
 @app.route('/about.html')
 def about():
-    return(render_template('about3.html'))
-@app.route('/Avant-Garde.html')
+    return render_template('about.html')
+@app.route('/Avant-Garde')
 def avant():
-    return(render_template('Avant-Garde.html'))
-@app.route('/Minimal.html')
+    con = connect()
+    query = """SELECT "Uri" FROM node_communities WHERE "NewGenre"='Avant-Garde' ORDER BY "popularity" LIMIT 20;"""
+    query_results = pd.read_sql_query(query, con)
+    subgenre = []
+    query_results_v = list(query_results['Uri'])
+
+    print query_results_v
+    for i in range(len(query_results_v)):
+        print query_results_v[i]
+        dict_song = mydict([['widget', base + query_results_v[i]], ['results_uri', '/results/' + query_results_v[i]]])
+        subgenre.append(dict_song)
+    return render_template('Avant-Garde.html', subgenre = subgenre)
+@app.route('/Minimal')
 def minimal():
-    return(render_template('Minimal.html'))
-@app.route('/Orchestral.html')
+    con = connect()
+    query = """SELECT "Uri" FROM node_communities WHERE "NewGenre"='Minimal' ORDER BY "popularity" LIMIT 20;"""
+    query_results = pd.read_sql_query(query, con)
+    subgenre = []
+    query_results_v = list(query_results['Uri'])
+
+    print query_results_v
+    for i in range(len(query_results_v)):
+        print query_results_v[i]
+        dict_song = mydict([['widget', base + query_results_v[i]], ['results_uri', '/results/' + query_results_v[i]]])
+        subgenre.append(dict_song)
+    return render_template('Minimal.html', subgenre = subgenre)
+@app.route('/Orchestral')
 def orchestral():
-    return(render_template('Orchestral.html'))
-@app.route('/Romantic.html')
+    con = connect()
+    query = """SELECT "Uri" FROM node_communities WHERE "NewGenre"='Orchestral' ORDER BY "popularity" LIMIT 20;"""
+    query_results = pd.read_sql_query(query, con)
+    subgenre = []
+    query_results_v = list(query_results['Uri'])
+
+    print query_results_v
+    for i in range(len(query_results_v)):
+        print query_results_v[i]
+        dict_song = mydict([['widget', base + query_results_v[i]], ['results_uri', '/results/' + query_results_v[i]]])
+        subgenre.append(dict_song)
+    return render_template('Orchestral.html', subgenre = subgenre)
+@app.route('/Romantic')
 def romantic():
-    return(render_template('Romantic.html'))
-@app.route('/Classical+Period.html')
+    con = connect()
+    query = """SELECT "Uri" FROM node_communities WHERE "NewGenre"='Romantic' ORDER BY "popularity" LIMIT 20;"""
+    query_results = pd.read_sql_query(query, con)
+    subgenre = []
+    query_results_v = list(query_results['Uri'])
+
+    print query_results_v
+    for i in range(len(query_results_v)):
+        print query_results_v[i]
+        dict_song = mydict([['widget', base + query_results_v[i]], ['results_uri', '/results/' + query_results_v[i]]])
+        subgenre.append(dict_song)
+    return render_template('Romantic.html', subgenre = subgenre)
+@app.route('/Classical+Period')
 def classical():
-    return(render_template('Classical+Period.html'))
-@app.route('/Early+Music.html')
+    con = connect()
+    query = """SELECT "Uri" FROM node_communities WHERE "NewGenre"='Classical Period' ORDER BY "popularity" LIMIT 20;"""
+    query_results = pd.read_sql_query(query, con)
+    subgenre = []
+    query_results_v = list(query_results['Uri'])
+
+    print query_results_v
+    for i in range(len(query_results_v)):
+        print query_results_v[i]
+        dict_song = mydict([['widget', base + query_results_v[i]], ['results_uri', '/results/' + query_results_v[i]]])
+        subgenre.append(dict_song)
+    return render_template('Classical+Period.html', subgenre = subgenre)
+@app.route('/Early+Music')
 def earlymusic():
-    return(render_template('Early+Music.html'))
-@app.route('/Chant.html')
-def Chant():
-    return(render_template('Chant.html'))
-@app.route('/Opera.html')
+    con = connect()
+    query = """SELECT "Uri" FROM node_communities WHERE "NewGenre"='Early Music' ORDER BY "popularity" LIMIT 20;"""
+    query_results = pd.read_sql_query(query, con)
+    subgenre = []
+    query_results_v = list(query_results['Uri'])
+
+    print query_results_v
+    for i in range(len(query_results_v)):
+        print query_results_v[i]
+        dict_song = mydict([['widget', base + query_results_v[i]], ['results_uri', '/results/' + query_results_v[i]]])
+        subgenre.append(dict_song)
+    return render_template('Early+Music.html', subgenre = subgenre)
+@app.route('/Opera')
 def Opera():
-    return(render_template('Opera.html'))
-@app.route('/Baroque.html')
+    con = connect()
+    query = """SELECT "Uri" FROM node_communities WHERE "NewGenre"='Opera' ORDER BY "popularity" LIMIT 20;"""
+    query_results = pd.read_sql_query(query, con)
+    subgenre = []
+    query_results_v = list(query_results['Uri'])
+
+    print query_results_v
+    for i in range(len(query_results_v)):
+        print query_results_v[i]
+        dict_song = mydict([['widget', base + query_results_v[i]], ['results_uri', '/results/' + query_results_v[i]]])
+        subgenre.append(dict_song)
+    return render_template('Opera.html', subgenre = subgenre)
+@app.route('/Baroque')
 def baroque():
-    return(render_template('Baroque.html'))
-@app.route('/Renaissance.html')
+    con = connect()
+    query = """SELECT "Uri" FROM node_communities WHERE "NewGenre"='Baroque' ORDER BY "popularity" LIMIT 20;"""
+    query_results = pd.read_sql_query(query, con)
+    subgenre = []
+    query_results_v = list(query_results['Uri'])
+
+    print query_results_v
+    for i in range(len(query_results_v)):
+        print query_results_v[i]
+        dict_song = mydict([['widget', base + query_results_v[i]], ['results_uri', '/results/' + query_results_v[i]]])
+        subgenre.append(dict_song)
+    return render_template('Baroque.html', subgenre = subgenre)
+@app.route('/Renaissance')
 def ren():
-    return(render_template('Renaissance.html'))
+    con = connect()
+    query = """SELECT "Uri" FROM node_communities WHERE "NewGenre"='Renaissance' ORDER BY "popularity" LIMIT 20;"""
+    query_results = pd.read_sql_query(query, con)
+    subgenre = []
+    query_results_v = list(query_results['Uri'])
+
+    print query_results_v
+    for i in range(len(query_results_v)):
+        print query_results_v[i]
+        dict_song = mydict([['widget', base + query_results_v[i]], ['results_uri', '/results/' + query_results_v[i]]])
+        subgenre.append(dict_song)
+    return render_template('Renaissance.html', subgenre = subgenre)
+@app.route('/results/<song_uri>')
+def get_song_uri(song_uri):
+    previous_song = base + song_uri.replace('/results', '')
+    print previous_song
+    previous_song = dict({'uri': previous_song})
+
+    query = text("""SELECT "Community" FROM node_communities WHERE "Uri" = :ge""").bindparams(ge=(song_uri))
+    return render_template('results.html', previous_song = previous_song)
+
